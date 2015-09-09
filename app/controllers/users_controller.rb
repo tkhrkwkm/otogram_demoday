ab1614057d58b1a0feffaddf78107bb2ce6595a8
tkawakami <tkawakami@tkltd.jp>
2015-09-09 12:01:08 +0000

    add settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show
    @user = User.find(params[:id])
   end
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
+    @user = User.new(signup_params)
     if @user.save
       flash[:success] = 'Welcome to the Otogram!!'
       session[:user_id] = @user.id
@@ -18,9 +20,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    if current_user != User.find(params[:id])
+      redirect_to user_path(current_user)
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを編集しました。"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
 
   private
-  def user_params
+  def signup_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
 end
